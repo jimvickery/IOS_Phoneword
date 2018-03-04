04638795c4cd73e671f050ca0dcc0d0c392c8416
Jim Vickery <jimrv8@yahoo.com>
2018-03-04 01:14:23 -0600

    Added code to convert alphanumeric to numbers.

--- Phoneword_iOS/ViewController.cs
+++ Phoneword_iOS/ViewController.cs
@@ -13,8 +13,30 @@ namespace Phoneword_iOS
 
         public override void ViewDidLoad()
         {
-            base.ViewDidLoad();
-            // Perform any additional setup after loading the view, typically from a nib.
+            string translatedNumber = "";
+
+            TranslateButton.TouchUpInside += (object sender, EventArgs e) => {
+                // Convert the phone number with text to a number
+                // using PhoneTranslator.cs
+                translatedNumber = PhoneTranslator.ToNumber(
+                    PhoneNumberText.Text);
+
+                // Dismiss the keyboard if text field was tapped
+                PhoneNumberText.ResignFirstResponder();
+
+                if (translatedNumber == "")
+                {
+                    CallButton.SetTitle("Call ", UIControlState.Normal);
+                    CallButton.Enabled = false;
+                }
+                else
+                {
+                    CallButton.SetTitle("Call " + translatedNumber,
+                        UIControlState.Normal);
+                    CallButton.Enabled = true;
+                }
+            };
+
         }
 
         public override void DidReceiveMemoryWarning()
